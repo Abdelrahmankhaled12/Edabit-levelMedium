16a81b4b5bd4c2b843e1c1f5383a04596527509d
Abdelrahman khaled <108089479+Abdelrahmankhaled12@users.noreply.github.com>
2022-10-07 14:14:42 +0200

    Add files via upload

---
+++ Convert Key, Values in an Object to Array/main.js
@@ -0,0 +1,12 @@
+function objectToArray(obj) {
+	return Object.entries(obj)
+}
+
+
+
+
+console.log(objectToArray({
+    D: 1,
+    B: 2,
+    C: 3
+  }))
---
+++ Integer Digits Count/main.js
@@ -0,0 +1,9 @@
+function count(n) {
+	return Math.abs(n).toString().split("").length
+}
+
+console.log(count(318))
+console.log(count(-92563))
+console.log(count(4666))
+console.log(count(-314890))
+console.log(count(654321))
