1cb22b9c53226d90ee78822f0deb9a5c1b39569b
Abdelrahman khaled <108089479+Abdelrahmankhaled12@users.noreply.github.com>
2022-10-08 13:05:07 +0200

    Add files via upload

---
+++ Remove Trailing and Leading Zeros/main.js
@@ -0,0 +1,24 @@
+function removeLeadingTrailing(n) {
+    return `${+n}`
+}
+
+
+
+console.log(removeLeadingTrailing("230.000"))
+console.log(removeLeadingTrailing("00402"))
+console.log(removeLeadingTrailing("03.1400"))
+console.log(removeLeadingTrailing("30"))
+console.log(removeLeadingTrailing("30.0000"))
+console.log(removeLeadingTrailing("24340"))
+console.log(removeLeadingTrailing("0404040"))
+console.log(removeLeadingTrailing("0"))
+console.log(removeLeadingTrailing("00"))
+console.log(removeLeadingTrailing("0.000000"))
+console.log(removeLeadingTrailing("0000.000"))
+console.log(removeLeadingTrailing("00.0001"))
+console.log(removeLeadingTrailing("10000"))
+console.log(removeLeadingTrailing("1345"))
+console.log(removeLeadingTrailing("30.000020"))
+console.log(removeLeadingTrailing("00200.1900001"))
+
+
---
+++ Sort the Unsortable/main.js
@@ -0,0 +1,10 @@
+function sortIt(arr) {
+	return arr.sort()
+}
+
+console.log(sortIt([4, 1, 3]))
+console.log(sortIt([[4], [1], [3]]))
+console.log(sortIt([4, [1], 3]))
+console.log(sortIt([[4], 1, [3]]))
+console.log(sortIt([[3], 4, [2], [5], 1, 6]))
+
